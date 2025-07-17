8dd0ec860fa8bfd5be48f8f6091e6c24783e83b0
KyleBrindle <kylegbrindle@gmail.com>
2025-07-17 15:59:19 -0400

    Made association rules easier to read by removing frozenset from result

--- utils/apriori.py
+++ utils/apriori.py
@@ -4,7 +4,9 @@ import pandas as pd
 def perform_apriori(transactions):
     all_items = sorted(set(item for t in transactions for item in t))
     df = pd.DataFrame([{item: (item in t) for item in all_items} for t in transactions])
-    frequent = apriori(df, min_support=0.2, use_colnames=True)
-    rules = association_rules(frequent, metric="lift", min_threshold=1.0)
+    frequent = apriori(df, min_support=0.25, use_colnames=True)
+    rules = association_rules(frequent, metric="lift", min_threshold=1.5)
     rules = rules.round({'lift' : 4, 'confidence' : 4})
+    rules['antecedents'] = rules['antecedents'].apply(list)
+    rules['consequents'] = rules['consequents'].apply(list)
     return rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']].to_dict('records')
